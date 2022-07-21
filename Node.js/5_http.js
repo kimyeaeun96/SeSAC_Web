780ff85a4d284571d1c2efb793ec5fcf4fc69e90
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-07-21 19:58:38 +0900

    Update 5_http.js

--- Node.js/5_http.js
+++ Node.js/5_http.js
@@ -6,8 +6,8 @@ const server = http.createServer( function(req, res){
     res.end("<p>안녕</p>"); // response를 해당 내용으로 "보낸다"
 }); // 서버 생성
 
-server.listen(8000, function(){
-    console.log("8000번 포트");   // ACG 80
+server.listen(8080, function(){
+    console.log("8080번 포트");   // ACG 80
 })
 
 server.on( "request", function(){
