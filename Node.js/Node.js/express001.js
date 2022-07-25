ef20667855e0ae9c0c7a1a80a0c1bf94c72f11ee
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-07-25 10:08:26 +0900

    Study: Nodejs

--- Node.js/Node.js/express001.js
+++ Node.js/Node.js/express001.js
@@ -3,13 +3,15 @@ const app = express();
 const port = 8080;
 
 app.set("view engine", "ejs"); //확장자가 ejs인 파일, view 폴더 안에 모아둔다.
-//app.use( express.static( 'public' )); // publc 폴더는 정적파일을 관리하는 곳으로 선언!
+//app.use( express.static( 'public' )); // publc 폴더는 정적파일을 관리하는 곳으로 선언! 이미지, html, css 정적파일
 app.use( '/abc/aa', express.static( 'public' ));  
-// '/abc' 가상경로를 통해서만 public 폴더에 접속하겠다.  
+// '/abc' 가상경로를 통해서만 public 폴더에 접속하겠다.
+// 가상경로 설정이유: 작업을 묶어주려고. 
 
 app.get('/', (req, res) => {
      // db에서 정보를 가져왔다.
-    res.render("prc29") // 확장자 쓸 필요 없다. 
+    var list = ['apple', 'banana']
+    res.render("test",{A:list}); // 확장자 쓸 필요 없다. 
 });
 
 app.listen(port, () => {
