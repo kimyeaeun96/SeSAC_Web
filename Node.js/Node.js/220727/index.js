c2d7bf4ecda93a4c7f464a4aee97cf23c7554334
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-07-28 22:50:11 +0900

    Study: nodejs app.get and app.post

--- Node.js/Node.js/220727/index.js
+++ Node.js/Node.js/220727/index.js
@@ -10,49 +10,32 @@ app.use( express.urlencoded({extended: true}));
 app.use( bodyParser.json() ); 
 
 app.get("/", function(req, res) {
-    res.render("prc32-1");          //뷰를 렌더링한다. 
+    res.render("prc32-1");          
 });
-
 app.get("/first", function(req, res) {
-    res.render("prc33");          
-});
-
-app.get("/test", function(req, res) {
-    res.send("test");            // 응답으로 데이터 보낸다. 그래야 클라이언트화면에서 요청 처리가 끝났음을 안다. 
-});
-
-app.get("/test2", function(req, res) {
-    res.send({name: "test", message: 123});
-});
-/*
-app.get("/receive", function(req, res){
-    console.log(req.query);
-    let name = req.query.name;
-    let msg = req.query.name + "GET";
-    res.send( { name: name, message: msg } );
+    res.render("prc33");         
 });
 
 app.get("/first/receive", function(req, res){
     console.log(req.query);
-    let name = req.query.name;
-    let msg = req.query.name + "GET";
-    res.send( { name: name, message: msg } );
+    let name = req.query.name  + "GET";
+    let gender = req.query.gender + "GET";
+    let year = req.query.year + "GET";
+    res.send( { name: name, gender: gender, year: year } );
 });
-*/
+
 app.post('/', function(req, res){
     let string = fs.readFileSync("./newinfo/info.txt").toString();
-    console.log(string);
     let strArray = string.split("//")
-    console.log(strArray[0], strArray[1], strArray[2])
 
     let ID = req.body.id
     let PW = req.body.pw
     if (ID === strArray[0] && PW === strArray[1]) {
         //로그인 성공시
-        res.render("./success")
+        res.render("success")
     }else {
         //로그인 실패시
-        res.render("./failure")
+        res.render("failure")
     }}
 )
 
