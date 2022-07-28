2b6e4557eb79fcfa747ec9b3c1e6b8f816ad2978
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-07-28 23:02:41 +0900

    Delete queryString.js

--- Node.js/queryString.js
+++
@@ -1,8 +0,0 @@
-const url = require("url");
-const querystring = require("querystring");
-const string = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=sesac&oquery=sesac&tqi=hWg%2F8sprvmZss6uZF3GssssssfV-461107";
-
-const parsedUrl = url.parse(string);
-const query = querystring.parse(parsedUrl.query);
-console.log("querystring.parse(): ", query);
-console.log("querystring.stringify(): ", querystring.stringify(query)); 
