4362e98935c8274b71be8053d629d13b9cc349af
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-08-09 22:36:24 +0900

    Study: nodejs MVC CRUD

--- Node.js/Node.js/220805/model/Visitor.js
+++ Node.js/Node.js/220805/model/Visitor.js
@@ -14,7 +14,7 @@ exports.get_visitors = (cb) => {    // cnn 콜백함수
     });
 }
 exports.insert = ( name, comment, cb ) => {
-    var sql = "INSERT INTO visitor(name, comment) VALUES ('" + name + "', '" +comment + "')";
+    var sql = "INSERT INTO visitor(name, comment) VALUES ('" + name + "', '" + comment + "')";
     cnn.query( sql, (err, rows) => {
         if ( err ) throw err;
 
@@ -23,3 +23,31 @@ exports.insert = ( name, comment, cb ) => {
     });
 }
 
+exports.get_visitor = (id, cb) => {
+    // id 컬럼의 값이 id인 데이터를 1개만 검색한다. 
+    console.log(id);
+    cnn.query( `SELECT * FROM visitor where ID = '${id}' limit 1;`, (err, rows) => {
+        if ( err ) throw err;
+        console.log( rows )
+        cb(rows);
+    });
+}
+
+exports.update = (data, cb) => {
+    let sql = `UPDATE visitor SET name='${data.name}', comment='${data.comment}' WHERE id='${data.id}`;
+    // data.id 로 넘어온 값의 name, comment 업데이트 
+    cnn.query( sql, ( err, rows ) => {
+        if ( err ) throw err;
+        cb(rows);
+    })
+}
+
+exports.delete = (id, cb) => {
+    console.log( id );
+    let sql = `DELETE FROM visitor WHERE id=${id}`;
+    console.log( sql );
+    cnn.query( sql, (err, rows) => {
+        if ( err ) throw err;
+        cb(rows);
+    });
+}
