4f8a8169902fe3e1aaf17ad63ffff7bee26392e5
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-08-09 22:35:52 +0900

    Study: nodejs MVC CRUD

--- Node.js/Node.js/220805/routes/index.js
+++ Node.js/Node.js/220805/routes/index.js
@@ -4,5 +4,8 @@ const visitor = require("../controller/VisitorController");
 
 router.get("/", visitor.get_visitors);
 router.post("/write", visitor.post_comment);
+router.get("/get", visitor.get_visitor);
+router.patch("/edit", visitor.patch_comment);
+router.delete("/delete", visitor.delete_comment);
 
 module.exports = router;
