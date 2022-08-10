8f2409462f6548caaa291e9c533d4e6d82d0ad2f
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-08-10 22:03:38 +0900

    Study: nodejs sequelize

---
+++ Node.js/Node.js/220810/model/index.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+const config = require("../config/config.json")["development"];
+
+const db = {};
+const sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.Visitor = require("./Visitor")(sequelize, Sequelize);
+//const a = require("./Visior")
+//const b = a(sequelize, Sequelize);
+
+module.exports = db;
