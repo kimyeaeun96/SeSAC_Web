2bbfd642956718de02e9374e6def59d39ca78d00
kimyeaeun96 <mangokim96@gmail.com>
2022-11-11 18:19:36 +0900

    Study: spring 설치, 프로젝트생성

---
+++ Spring/sesac-spring-study/src/main/java/sesac/spring/study/sesacspringstudy/controller/HelloController.java
@@ -0,0 +1,19 @@
+package sesac.spring.study.sesacspringstudy.controller;
+
+import lombok.extern.flogger.Flogger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // annotation 컨트롤러의 역할을 스프링에게 알려준다. 꼭 써줘야 실행됨!!!
+public class HelloController {
+    @GetMapping("hi") // get URL hi
+    public String getHi(Model model){ // 톰캣서버가 모델에서 정보를 받고 view로 보낸다.
+        model.addAttribute("text", "hihihi"); // th:text="${text}"
+        return "hi"; // [template] - hi.html
+    }
+    @GetMapping("hello")
+    public String getHi2(){
+        return "hi2";
+    }
+}
